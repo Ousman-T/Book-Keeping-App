0c436e3b644ddb42df419e42e8c871b44f56162b
Ousman Touray <otouray14@gmail.com>
2023-09-21 12:12:40 -0400

    created a custom exception example

---
+++ src/main/java/com/development/bookkeeping/exception/ApiExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.development.bookkeeping.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+
+//        1. Create payload containing exception details to send in response
+
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                e,
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("America/New_York"))
+        );
+
+//        2. Return the actual response entity.
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+}
